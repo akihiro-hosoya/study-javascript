fa3b7c0669a99eac9fdeaaa70af1f59dcd3d7cd5
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-02-24 18:53:22 +0900

    移動

---
+++ ES6/index.js
@@ -0,0 +1,17 @@
+// OLD JAVASCRIPT
+
+var counter = 5;
+
+function sayName(){
+    var name = 'Ed';
+    console.log(name);
+}
+
+console.log(name); // erorr
+
+sayName();
+
+// ES6
+const todoList = ["Milk", 'Cow'];
+
+todoList = ["Milk"];
