d0b3064668bba4fc9da44e7a543ef806a9299584
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-02-26 21:17:14 +0900

    test

---
+++ Let's_program/app.js
@@ -0,0 +1,15 @@
+document.write("<p>UTF-8で記述された外部ファイル</p>");
+
+let num = 10;
+document.write("<p>" + num + "</p>");
+
+// 一行コメント
+/* 一行コメント */
+/*
+複数行に渡る
+コメント
+*/
+
+alert("こんにちは\nお元気ですか");
+
+document.write("<p>")
