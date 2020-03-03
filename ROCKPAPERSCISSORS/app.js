803a3f9d26de238060083ddcd8e377bcdd2847a3
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-03-03 19:22:35 +0900

    変更

--- ROCKPAPERSCISSORS/app.js
+++ ROCKPAPERSCISSORS/app.js
@@ -20,28 +20,28 @@ const game = ()=> {
         const computerHand = document.querySelector('.computer-hand');
         const hands = document.querySelectorAll('.hands img');
 
-        hands.forEach(hand =>{
-            hand.addEventListener('animationend', () => {
+        hands.forEach(hand => {
+            hand.addEventListener('animationend', function() {
                 this.style.animation = "";
             });
         });
         // Computer Option
         const computerOptions = ['rock', 'paper', 'scissors'];
 
-        options.forEach((option)=>{
-            option.addEventListener('click', () => {
+        options.forEach( option => {
+            option.addEventListener('click', function() {
                 // Computer choice
                 const computerNumber = Math.floor(Math.random() * 3);
                 const computerChoice = computerOptions[computerNumber];
                 
-                setTimeout(()=>{
+                setTimeout(() => {
                     // Here is where we call compare hands
                     compareHands(this.textContent, computerChoice);
                     // Update Images
                     playerHand.src = `./assets/${this.textContent}.png`;
                     computerHand.src = `./assets/${computerChoice}.png`;
                 
-                }, 2000)
+                }, 2000);
                 // Animation
                 playerHand.style.animation = "shakePlayer 2s ease";
                 computerHand.style.animation = "shakeComputer 2s ease";
@@ -49,17 +49,18 @@ const game = ()=> {
         });
     };
 
-    const updateScore = () =>{
+    const updateScore = function() {
         const playerScore = document.querySelector('.player-score p');
         const computerScore = document.querySelector('.computer-score p');
         playerScore.textContent = pScore;
         computerScore.textContent = cScore;
     };
 
-    const compareHands = (playerChoice, computerChoice) =>{
+    const compareHands = function(playerChoice, computerChoice) {
         // Update Text
         const winner = document.querySelector('.winner')
-        if(playerChoice === computerChoice){
+        // Checking for a tie
+        if(playerChoice === computerChoice) {
             winner.textContent = 'It is a tie';
             return;
         }
